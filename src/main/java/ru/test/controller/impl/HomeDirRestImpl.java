fbd8d14a2df8143cc72f242181b4ba98a21b262b
ksander301 <sanek301@rambler.ru>
2019-11-04 22:02:52 +0400

    222

--- src/main/java/ru/test/controller/impl/HomeDirRestImpl.java
+++ src/main/java/ru/test/controller/impl/HomeDirRestImpl.java
@@ -2,19 +2,15 @@ package ru.test.controller.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.*;
 import ru.test.controller.HomeDirRest;
 import ru.test.logic.HomeDirService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.nio.charset.Charset;
 import java.util.List;
 
 
